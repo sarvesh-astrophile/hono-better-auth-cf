662eb5f7ca6dc44016191106c1506eaba0bc6314
sarvesh-astrophile <sarvesh9301@gmail.com>
2025-08-30 18:58:44 +0530

    Refactor authentication setup and API routes
    - Removed previous user-related endpoints and replaced them with a unified authentication handler.
    - Introduced `createAuth` function in `lib/auth.ts` to encapsulate Better Auth setup with Drizzle adapter and OpenAPI plugin.
    - Updated main application entry point in `index.ts` to utilize the new authentication handler for API routes.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,16 @@
 import { Hono } from "hono";
-import { db } from "@/db/db";
-import { user } from "@/db/schema";
+import { createAuth } from "./lib/auth";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
-});
-
-app.get("/users", async (c) => {
-	const dbInstance = db(c.env.hono_better_auth_cf);
-	const result = await dbInstance.select().from(user).all();
-	return c.json(result);
-});
-
-app.get("/add-users", async (c) => {
-	const dbInstance = db(c.env.hono_better_auth_cf);
-	const result = await dbInstance.insert(user).values({
-		id: crypto.randomUUID(),
-		name: "John Doe",
-		email: "john.doe@example.com",
+app
+	.on(['POST','GET'], '/api/auth/**', (c) => {
+		const auth = createAuth(c.env.hono_better_auth_cf);
+		return auth.handler(c.req.raw);
+	})
+	.get('/', (c) => {
+		return c.text("Hello Hono!");
 	});
-	return c.json(result);
-});
+
 
 export default app;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,19 @@
 import { db } from "@/db/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { openAPI } from "better-auth/plugins";
+import type { D1Database } from "@cloudflare/workers-types";
 
-export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "sqlite"
-  }),
-  emailAndPassword: {
-    enabled: true,
-  },
-});
+export function createAuth(d1Database: D1Database) {
+  return betterAuth({
+    database: drizzleAdapter(db(d1Database), {
+      provider: "sqlite"
+    }),
+    emailAndPassword: {
+      enabled: true,
+    },
+    plugins: [
+      openAPI(),
+    ],
+  });
+}
